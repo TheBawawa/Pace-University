3ba693744352fe69bc2798a99b883a5630890f7c
junjie <junjie@startdium.com>
2023-11-13 21:08:28 -0500

    Fall 2023

---
+++ Fall 2023/Intro to CS/Homework/Homework 05/hw5_q1.py
@@ -0,0 +1,10 @@
+"""
+Author: Ana Paredes
+Assignment / Part: HW5 - Q1
+Date due: 11/16/23, 11:59pm
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+Pace University Policies and Procedures on Academic
+Misconduct.
+"""
+
